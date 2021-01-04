04e39d71e19502b8585765d2d92e913b7a84cbe9
Fabrizzio Rivera <frivera@nevicad.com>
2021-01-04 15:40:03 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const port = process.env.PORT | 5000
+const http = require("http");
+  const server = http.createServer(function(req,res){
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({Resultado: 1}));
+});
+server.listen(port, function(){
+  console.log(`server listening on port ${port}`);
+});
